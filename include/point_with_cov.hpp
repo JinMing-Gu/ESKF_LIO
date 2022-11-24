ca545aeb84a582b3ebab90c62626656191ef3fd8
gjm10775 <jinming.gu@uisee.com>
2022-11-24 16:48:09 +0800

    code format

--- include/point_with_cov.hpp
+++ include/point_with_cov.hpp
@@ -42,97 +42,97 @@
 // pcl point type: https://github.com/PointCloudLibrary/pcl/blob/master/common/include/pcl/impl/point_types.hpp
 namespace pcl
 {
-     struct EIGEN_ALIGN16 _PointXYZIWithCov
-     {
-          PCL_ADD_POINT4D; // preferred way of adding a XYZ+padding
-          float intensity;
-          float cov_vec[6]; // cxx, cxy, cxz, cyy, cyz, czz
-          float cov_trace;
+    struct EIGEN_ALIGN16 _PointXYZIWithCov
+    {
+        PCL_ADD_POINT4D; // preferred way of adding a XYZ+padding
+        float intensity;
+        float cov_vec[6]; // cxx, cxy, cxz, cyy, cyz, czz
+        float cov_trace;
 
-          EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-     } EIGEN_ALIGN16;
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    } EIGEN_ALIGN16;
 
-     struct PointXYZIWithCov : public _PointXYZIWithCov
-     {
-          inline PointXYZIWithCov()
-          {
-               x = y = z = 0.0f;
-               intensity = 0;
-               cov_vec[0] = cov_vec[1] = cov_vec[2] = cov_vec[3] = cov_vec[4] = cov_vec[5] = 0.0f;
-               cov_trace = 0.0f;
-          }
+    struct PointXYZIWithCov : public _PointXYZIWithCov
+    {
+        inline PointXYZIWithCov()
+        {
+            x = y = z = 0.0f;
+            intensity = 0;
+            cov_vec[0] = cov_vec[1] = cov_vec[2] = cov_vec[3] = cov_vec[4] = cov_vec[5] = 0.0f;
+            cov_trace = 0.0f;
+        }
 
-          inline PointXYZIWithCov(float _x, float _y, float _z, float _intensity,
-                                  float _cov_xx, float _cov_xy, float _cov_xz,
-                                  float _cov_yy, float _cov_yz, float _cov_zz)
-          {
-               x = _x;
-               y = _y;
-               z = _z;
-               intensity = _intensity;
-               cov_vec[0] = _cov_xx;
-               cov_vec[1] = _cov_xy;
-               cov_vec[2] = _cov_xz;
-               cov_vec[3] = _cov_yy;
-               cov_vec[4] = _cov_yz;
-               cov_vec[5] = _cov_zz;
-               cov_trace = _cov_xx + _cov_yy + _cov_zz;
-          }
+        inline PointXYZIWithCov(float _x, float _y, float _z, float _intensity,
+                                float _cov_xx, float _cov_xy, float _cov_xz,
+                                float _cov_yy, float _cov_yz, float _cov_zz)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+            intensity = _intensity;
+            cov_vec[0] = _cov_xx;
+            cov_vec[1] = _cov_xy;
+            cov_vec[2] = _cov_xz;
+            cov_vec[3] = _cov_yy;
+            cov_vec[4] = _cov_yz;
+            cov_vec[5] = _cov_zz;
+            cov_trace = _cov_xx + _cov_yy + _cov_zz;
+        }
 
-          inline PointXYZIWithCov(const _PointXYZIWithCov &p)
-          {
-               x = p.x;
-               y = p.y;
-               z = p.z;
-               intensity = p.intensity;
-               cov_vec[0] = p.cov_vec[0];
-               cov_vec[1] = p.cov_vec[1];
-               cov_vec[2] = p.cov_vec[2];
-               cov_vec[3] = p.cov_vec[3];
-               cov_vec[4] = p.cov_vec[4];
-               cov_vec[5] = p.cov_vec[5];
-               cov_trace = p.cov_trace;
-          }
+        inline PointXYZIWithCov(const _PointXYZIWithCov &p)
+        {
+            x = p.x;
+            y = p.y;
+            z = p.z;
+            intensity = p.intensity;
+            cov_vec[0] = p.cov_vec[0];
+            cov_vec[1] = p.cov_vec[1];
+            cov_vec[2] = p.cov_vec[2];
+            cov_vec[3] = p.cov_vec[3];
+            cov_vec[4] = p.cov_vec[4];
+            cov_vec[5] = p.cov_vec[5];
+            cov_trace = p.cov_trace;
+        }
 
-          inline PointXYZIWithCov(const PointXYZI &p, const Eigen::Matrix3f &cov_matrix)
-          {
-               x = p.x;
-               y = p.y;
-               z = p.z;
-               intensity = p.intensity;
-               cov_vec[0] = cov_matrix(0, 0);
-               cov_vec[1] = cov_matrix(0, 1);
-               cov_vec[2] = cov_matrix(0, 2);
-               cov_vec[3] = cov_matrix(1, 1);
-               cov_vec[4] = cov_matrix(1, 2);
-               cov_vec[5] = cov_matrix(2, 2);
-               cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
-          }
+        inline PointXYZIWithCov(const PointXYZI &p, const Eigen::Matrix3f &cov_matrix)
+        {
+            x = p.x;
+            y = p.y;
+            z = p.z;
+            intensity = p.intensity;
+            cov_vec[0] = cov_matrix(0, 0);
+            cov_vec[1] = cov_matrix(0, 1);
+            cov_vec[2] = cov_matrix(0, 2);
+            cov_vec[3] = cov_matrix(1, 1);
+            cov_vec[4] = cov_matrix(1, 2);
+            cov_vec[5] = cov_matrix(2, 2);
+            cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
+        }
 
-          inline PointXYZIWithCov(const pcl::PointXYZINormal &p, const Eigen::Matrix3f &cov_matrix)
-          {
-               x = p.x;
-               y = p.y;
-               z = p.z;
-               intensity = p.intensity;
-               cov_vec[0] = cov_matrix(0, 0);
-               cov_vec[1] = cov_matrix(0, 1);
-               cov_vec[2] = cov_matrix(0, 2);
-               cov_vec[3] = cov_matrix(1, 1);
-               cov_vec[4] = cov_matrix(1, 2);
-               cov_vec[5] = cov_matrix(2, 2);
-               cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
-          }
-          friend std::ostream &operator<<(std::ostream &out, const PointXYZIWithCov &p);
-     };
+        inline PointXYZIWithCov(const pcl::PointXYZINormal &p, const Eigen::Matrix3f &cov_matrix)
+        {
+            x = p.x;
+            y = p.y;
+            z = p.z;
+            intensity = p.intensity;
+            cov_vec[0] = cov_matrix(0, 0);
+            cov_vec[1] = cov_matrix(0, 1);
+            cov_vec[2] = cov_matrix(0, 2);
+            cov_vec[3] = cov_matrix(1, 1);
+            cov_vec[4] = cov_matrix(1, 2);
+            cov_vec[5] = cov_matrix(2, 2);
+            cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
+        }
+        friend std::ostream &operator<<(std::ostream &out, const PointXYZIWithCov &p);
+    };
 
-     inline std::ostream &operator<<(std::ostream &out, const PointXYZIWithCov &p)
-     {
-          out << "(" << p.x << ", " << p.y << ", " << p.z << ", " << p.intensity << ", "
-              << p.cov_vec[0] << ", " << p.cov_vec[1] << ", " << p.cov_vec[2] << ", "
-              << p.cov_vec[3] << ", " << p.cov_vec[4] << ", " << p.cov_vec[5] << ", " << p.cov_trace << ")";
-          return out;
-     }
+    inline std::ostream &operator<<(std::ostream &out, const PointXYZIWithCov &p)
+    {
+        out << "(" << p.x << ", " << p.y << ", " << p.z << ", " << p.intensity << ", "
+            << p.cov_vec[0] << ", " << p.cov_vec[1] << ", " << p.cov_vec[2] << ", "
+            << p.cov_vec[3] << ", " << p.cov_vec[4] << ", " << p.cov_vec[5] << ", " << p.cov_trace << ")";
+        return out;
+    }
 }
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(pcl::_PointXYZIWithCov,
@@ -142,100 +142,100 @@ POINT_CLOUD_REGISTER_POINT_WRAPPER(pcl::PointXYZIWithCov, pcl::_PointXYZIWithCov
 
 namespace common
 {
-     typedef pcl::PointXYZIWithCov PointIWithCov;
-     typedef pcl::PointCloud<PointIWithCov> PointICovCloud;
-     typedef PointICovCloud::Ptr PointICovCloudPtr;
-     typedef PointICovCloud::ConstPtr PointICovCloudConstPtr;
+    typedef pcl::PointXYZIWithCov PointIWithCov;
+    typedef pcl::PointCloud<PointIWithCov> PointICovCloud;
+    typedef PointICovCloud::Ptr PointICovCloudPtr;
+    typedef PointICovCloud::ConstPtr PointICovCloudConstPtr;
 
-     void removeCov(const pcl::PointXYZIWithCov &pi, pcl::PointXYZI &po)
-     {
-          po.x = pi.x;
-          po.y = pi.y;
-          po.z = pi.z;
-          po.intensity = pi.intensity;
-     }
+    void removeCov(const pcl::PointXYZIWithCov &pi, pcl::PointXYZI &po)
+    {
+        po.x = pi.x;
+        po.y = pi.y;
+        po.z = pi.z;
+        po.intensity = pi.intensity;
+    }
 
-     pcl::PointXYZI removeCov(const pcl::PointXYZIWithCov &pi)
-     {
-          pcl::PointXYZI po;
-          po.x = pi.x;
-          po.y = pi.y;
-          po.z = pi.z;
-          po.intensity = pi.intensity;
-          return po;
-     }
+    pcl::PointXYZI removeCov(const pcl::PointXYZIWithCov &pi)
+    {
+        pcl::PointXYZI po;
+        po.x = pi.x;
+        po.y = pi.y;
+        po.z = pi.z;
+        po.intensity = pi.intensity;
+        return po;
+    }
 
-     void appendCov(const pcl::PointXYZI &pi, pcl::PointXYZIWithCov &po, const Eigen::Matrix3d &cov_matrix)
-     {
-          po.x = pi.x;
-          po.y = pi.y;
-          po.z = pi.z;
-          po.intensity = pi.intensity;
-          po.cov_vec[0] = cov_matrix(0, 0);
-          po.cov_vec[1] = cov_matrix(0, 1);
-          po.cov_vec[2] = cov_matrix(0, 2);
-          po.cov_vec[3] = cov_matrix(1, 1);
-          po.cov_vec[4] = cov_matrix(1, 2);
-          po.cov_vec[5] = cov_matrix(2, 2);
-          po.cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
-     }
+    void appendCov(const pcl::PointXYZI &pi, pcl::PointXYZIWithCov &po, const Eigen::Matrix3d &cov_matrix)
+    {
+        po.x = pi.x;
+        po.y = pi.y;
+        po.z = pi.z;
+        po.intensity = pi.intensity;
+        po.cov_vec[0] = cov_matrix(0, 0);
+        po.cov_vec[1] = cov_matrix(0, 1);
+        po.cov_vec[2] = cov_matrix(0, 2);
+        po.cov_vec[3] = cov_matrix(1, 1);
+        po.cov_vec[4] = cov_matrix(1, 2);
+        po.cov_vec[5] = cov_matrix(2, 2);
+        po.cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
+    }
 
-     pcl::PointXYZIWithCov appendCov(const pcl::PointXYZI &pi, const Eigen::Matrix3d &cov_matrix)
-     {
-          pcl::PointXYZIWithCov po;
-          po.x = pi.x;
-          po.y = pi.y;
-          po.z = pi.z;
-          po.intensity = pi.intensity;
-          po.cov_vec[0] = cov_matrix(0, 0);
-          po.cov_vec[1] = cov_matrix(0, 1);
-          po.cov_vec[2] = cov_matrix(0, 2);
-          po.cov_vec[3] = cov_matrix(1, 1);
-          po.cov_vec[4] = cov_matrix(1, 2);
-          po.cov_vec[5] = cov_matrix(2, 2);
-          po.cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
-          return po;
-     }
+    pcl::PointXYZIWithCov appendCov(const pcl::PointXYZI &pi, const Eigen::Matrix3d &cov_matrix)
+    {
+        pcl::PointXYZIWithCov po;
+        po.x = pi.x;
+        po.y = pi.y;
+        po.z = pi.z;
+        po.intensity = pi.intensity;
+        po.cov_vec[0] = cov_matrix(0, 0);
+        po.cov_vec[1] = cov_matrix(0, 1);
+        po.cov_vec[2] = cov_matrix(0, 2);
+        po.cov_vec[3] = cov_matrix(1, 1);
+        po.cov_vec[4] = cov_matrix(1, 2);
+        po.cov_vec[5] = cov_matrix(2, 2);
+        po.cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
+        return po;
+    }
 
-     void updateCov(const pcl::PointXYZIWithCov &pi, pcl::PointXYZIWithCov &po, const Eigen::Matrix3d &cov_matrix)
-     {
-          po.x = pi.x;
-          po.y = pi.y;
-          po.z = pi.z;
-          po.intensity = pi.intensity;
-          po.cov_vec[0] = cov_matrix(0, 0);
-          po.cov_vec[1] = cov_matrix(0, 1);
-          po.cov_vec[2] = cov_matrix(0, 2);
-          po.cov_vec[3] = cov_matrix(1, 1);
-          po.cov_vec[4] = cov_matrix(1, 2);
-          po.cov_vec[5] = cov_matrix(2, 2);
-          po.cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
-     }
+    void updateCov(const pcl::PointXYZIWithCov &pi, pcl::PointXYZIWithCov &po, const Eigen::Matrix3d &cov_matrix)
+    {
+        po.x = pi.x;
+        po.y = pi.y;
+        po.z = pi.z;
+        po.intensity = pi.intensity;
+        po.cov_vec[0] = cov_matrix(0, 0);
+        po.cov_vec[1] = cov_matrix(0, 1);
+        po.cov_vec[2] = cov_matrix(0, 2);
+        po.cov_vec[3] = cov_matrix(1, 1);
+        po.cov_vec[4] = cov_matrix(1, 2);
+        po.cov_vec[5] = cov_matrix(2, 2);
+        po.cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
+    }
 
-     void updateCov(pcl::PointXYZIWithCov &po, const Eigen::Matrix3d &cov_matrix)
-     {
-          po.cov_vec[0] = cov_matrix(0, 0);
-          po.cov_vec[1] = cov_matrix(0, 1);
-          po.cov_vec[2] = cov_matrix(0, 2);
-          po.cov_vec[3] = cov_matrix(1, 1);
-          po.cov_vec[4] = cov_matrix(1, 2);
-          po.cov_vec[5] = cov_matrix(2, 2);
-          po.cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
-     }
+    void updateCov(pcl::PointXYZIWithCov &po, const Eigen::Matrix3d &cov_matrix)
+    {
+        po.cov_vec[0] = cov_matrix(0, 0);
+        po.cov_vec[1] = cov_matrix(0, 1);
+        po.cov_vec[2] = cov_matrix(0, 2);
+        po.cov_vec[3] = cov_matrix(1, 1);
+        po.cov_vec[4] = cov_matrix(1, 2);
+        po.cov_vec[5] = cov_matrix(2, 2);
+        po.cov_trace = cov_matrix(0, 0) + cov_matrix(1, 1) + cov_matrix(2, 2);
+    }
 
-     void extractCov(const pcl::PointXYZIWithCov &p, Eigen::Matrix3d &cov_matrix)
-     {
-          cov_matrix.setZero();
-          cov_matrix(0, 0) = p.cov_vec[0],
-                        cov_matrix(0, 1) = p.cov_vec[1],
-                        cov_matrix(0, 2) = p.cov_vec[2],
-                        cov_matrix(1, 0) = p.cov_vec[1],
-                        cov_matrix(1, 1) = p.cov_vec[3],
-                        cov_matrix(1, 2) = p.cov_vec[4],
-                        cov_matrix(2, 0) = p.cov_vec[2],
-                        cov_matrix(2, 1) = p.cov_vec[4],
-                        cov_matrix(2, 2) = p.cov_vec[5];
-     }
+    void extractCov(const pcl::PointXYZIWithCov &p, Eigen::Matrix3d &cov_matrix)
+    {
+        cov_matrix.setZero();
+        cov_matrix(0, 0) = p.cov_vec[0],
+                      cov_matrix(0, 1) = p.cov_vec[1],
+                      cov_matrix(0, 2) = p.cov_vec[2],
+                      cov_matrix(1, 0) = p.cov_vec[1],
+                      cov_matrix(1, 1) = p.cov_vec[3],
+                      cov_matrix(1, 2) = p.cov_vec[4],
+                      cov_matrix(2, 0) = p.cov_vec[2],
+                      cov_matrix(2, 1) = p.cov_vec[4],
+                      cov_matrix(2, 2) = p.cov_vec[5];
+    }
 
 }
 
--- include/pose.cpp
+++ include/pose.cpp
@@ -15,53 +15,53 @@
 
 Pose::Pose()
 {
-     q_ = Eigen::Quaterniond::Identity();
-     t_ = Eigen::Vector3d::Zero();
-     T_ = Eigen::Matrix4d::Identity();
-     td_ = 0;
-     cov_.setZero();
+    q_ = Eigen::Quaterniond::Identity();
+    t_ = Eigen::Vector3d::Zero();
+    T_ = Eigen::Matrix4d::Identity();
+    td_ = 0;
+    cov_.setZero();
 }
 
 Pose::Pose(const Pose &pose)
 {
-     q_ = pose.q_;
-     t_ = pose.t_;
-     T_ = pose.T_;
-     td_ = pose.td_;
-     cov_ = pose.cov_;
+    q_ = pose.q_;
+    t_ = pose.t_;
+    T_ = pose.T_;
+    td_ = pose.td_;
+    cov_ = pose.cov_;
 }
 
 Pose::Pose(const Eigen::Quaterniond &q, const Eigen::Vector3d &t, const double &td)
 {
-     q_ = q;
-     q_.normalize();
-     t_ = t;
-     T_.setIdentity();
-     T_.topLeftCorner<3, 3>() = q_.toRotationMatrix();
-     T_.topRightCorner<3, 1>() = t_;
-     td_ = td;
-     cov_.setZero();
+    q_ = q;
+    q_.normalize();
+    t_ = t;
+    T_.setIdentity();
+    T_.topLeftCorner<3, 3>() = q_.toRotationMatrix();
+    T_.topRightCorner<3, 1>() = t_;
+    td_ = td;
+    cov_.setZero();
 }
 
 Pose::Pose(const Eigen::Matrix3d &R, const Eigen::Vector3d &t, const double &td)
 {
-     q_ = Eigen::Quaterniond(R);
-     t_ = t;
-     T_.setIdentity();
-     T_.topLeftCorner<3, 3>() = R;
-     T_.topRightCorner<3, 1>() = t_;
-     td_ = td;
-     cov_.setZero();
+    q_ = Eigen::Quaterniond(R);
+    t_ = t;
+    T_.setIdentity();
+    T_.topLeftCorner<3, 3>() = R;
+    T_.topRightCorner<3, 1>() = t_;
+    td_ = td;
+    cov_.setZero();
 }
 
 Pose::Pose(const Eigen::Matrix4d &T, const double &td)
 {
-     q_ = Eigen::Quaterniond(T.topLeftCorner<3, 3>());
-     q_.normalize();
-     t_ = T.topRightCorner<3, 1>();
-     T_ = T;
-     td_ = td;
-     cov_.setZero();
+    q_ = Eigen::Quaterniond(T.topLeftCorner<3, 3>());
+    q_.normalize();
+    t_ = T.topRightCorner<3, 1>();
+    T_ = T;
+    td_ = td;
+    cov_.setZero();
 }
 
 // Pose::Pose(const nav_msgs::Odometry &odom)
@@ -101,43 +101,43 @@ Pose::Pose(const Eigen::Matrix4d &T, const double &td)
 
 Pose Pose::poseTransform(const Pose &pose1, const Pose &pose2)
 {
-     // t12 = t1 + q1 * t2;
-     // q12 = q1 * q2;
-     return Pose(pose1.q_ * pose2.q_, pose1.q_ * pose2.t_ + pose1.t_);
+    // t12 = t1 + q1 * t2;
+    // q12 = q1 * q2;
+    return Pose(pose1.q_ * pose2.q_, pose1.q_ * pose2.t_ + pose1.t_);
 }
 
 Pose Pose::inverse() const
 {
-     return Pose(q_.inverse(), -(q_.inverse() * t_));
+    return Pose(q_.inverse(), -(q_.inverse() * t_));
 }
 
 void Pose::update()
 {
-     T_.topLeftCorner<3, 3>() = q_.toRotationMatrix();
-     T_.topRightCorner<3, 1>() = t_;
+    T_.topLeftCorner<3, 3>() = q_.toRotationMatrix();
+    T_.topRightCorner<3, 1>() = t_;
 }
 
 Pose Pose::operator*(const Pose &pose)
 {
-     return Pose(q_ * pose.q_, q_ * pose.t_ + t_);
+    return Pose(q_ * pose.q_, q_ * pose.t_ + t_);
 }
 
 ostream &operator<<(ostream &out, const Pose &pose)
 {
-     out << std::fixed << std::setprecision(3)
-         << "t [" << pose.t_(0) << "," << pose.t_(1) << "," << pose.t_(2)
-         << "], q [" << pose.q_.x() << "," << pose.q_.y() << "," << pose.q_.z() << "," << pose.q_.w()
-         << "], td [" << pose.td_ << "]";
-     return out;
+    out << std::fixed << std::setprecision(3)
+        << "t [" << pose.t_(0) << "," << pose.t_(1) << "," << pose.t_(2)
+        << "], q [" << pose.q_.x() << "," << pose.q_.y() << "," << pose.q_.z() << "," << pose.q_.w()
+        << "], td [" << pose.td_ << "]";
+    return out;
 }
 
 Eigen::Matrix<double, 6, 1> Pose::se3() const
 {
-     Sophus::SE3d SE3_qt(q_, t_);
-     Eigen::Matrix<double, 6, 1> xi = SE3_qt.log();
-     // Sophus::SE3::hat(xi)
-     // Sophus::SE3::vee(Sophus::SE3::hat(xi)) == xi
-     return xi;
+    Sophus::SE3d SE3_qt(q_, t_);
+    Eigen::Matrix<double, 6, 1> xi = SE3_qt.log();
+    // Sophus::SE3::hat(xi)
+    // Sophus::SE3::vee(Sophus::SE3::hat(xi)) == xi
+    return xi;
 }
 
 // quaternion averaging: https://wiki.unity3d.com/index.php/Averaging_Quaternions_and_Vectors
@@ -147,47 +147,47 @@ Eigen::Matrix<double, 6, 1> Pose::se3() const
 void computeMeanPose(const std::vector<std::pair<double, Pose>> &pose_array,
                      Pose &pose_mean, Eigen::Matrix<double, 6, 6> &pose_cov)
 {
-     // Solution 1: approximation if the separate quaternions are relatively close to each other.
-     // Solution 2: compute the mean in se3
-     pose_cov.setZero();
-     if (pose_array.size() == 1)
-     {
-          pose_mean = pose_array[0].second;
-          return;
-     }
+    // Solution 1: approximation if the separate quaternions are relatively close to each other.
+    // Solution 2: compute the mean in se3
+    pose_cov.setZero();
+    if (pose_array.size() == 1)
+    {
+        pose_mean = pose_array[0].second;
+        return;
+    }
 
-     const std::vector<std::pair<double, Pose>> pose_array_filter(
-         pose_array.begin() + size_t(pose_array.size() / 3), pose_array.end());
+    const std::vector<std::pair<double, Pose>> pose_array_filter(
+        pose_array.begin() + size_t(pose_array.size() / 3), pose_array.end());
 
-     // double weight_total = 0;
-     // Eigen::Matrix<double, 6, 1> xi_total = Eigen::Matrix<double, 6, 1>::Zero();
-     // Eigen::Matrix<double, 6, 1> xi_mean = xi_total / weight_total;
-     Eigen::Matrix<double, 6, 1> xi_mean = Eigen::Matrix<double, 6, 1>::Zero();
-     for (auto iter = pose_array_filter.begin(); iter != pose_array_filter.end(); iter++)
-     {
-          // weight_total += iter->first;
-          // xi_total += iter->first * iter->second.se3();
-          // std::cout << iter->first << ", " << iter->second << std::endl;
-          // LOG(INFO) << iter->first << ", " << iter->second;
-          std::cout << iter->second << std::endl;
-          // LOG(INFO) << iter->second;
-          xi_mean += iter->second.se3() / pose_array_filter.size();
-     }
-     pose_mean = Pose(Sophus::SE3d::exp(xi_mean).matrix());
+    // double weight_total = 0;
+    // Eigen::Matrix<double, 6, 1> xi_total = Eigen::Matrix<double, 6, 1>::Zero();
+    // Eigen::Matrix<double, 6, 1> xi_mean = xi_total / weight_total;
+    Eigen::Matrix<double, 6, 1> xi_mean = Eigen::Matrix<double, 6, 1>::Zero();
+    for (auto iter = pose_array_filter.begin(); iter != pose_array_filter.end(); iter++)
+    {
+        // weight_total += iter->first;
+        // xi_total += iter->first * iter->second.se3();
+        // std::cout << iter->first << ", " << iter->second << std::endl;
+        // LOG(INFO) << iter->first << ", " << iter->second;
+        std::cout << iter->second << std::endl;
+        // LOG(INFO) << iter->second;
+        xi_mean += iter->second.se3() / pose_array_filter.size();
+    }
+    pose_mean = Pose(Sophus::SE3d::exp(xi_mean).matrix());
 
-     for (auto iter = pose_array_filter.begin(); iter != pose_array_filter.end(); iter++)
-     {
-          Eigen::Matrix<double, 6, 1> xi = iter->second.se3();
-          // pose_cov += iter->first * iter->first * (xi - xi_mean) * (xi - xi_mean).transpose();
-          pose_cov += (pose_array_filter.size() - 1) * (xi - xi_mean) * (xi - xi_mean).transpose();
-     }
+    for (auto iter = pose_array_filter.begin(); iter != pose_array_filter.end(); iter++)
+    {
+        Eigen::Matrix<double, 6, 1> xi = iter->second.se3();
+        // pose_cov += iter->first * iter->first * (xi - xi_mean) * (xi - xi_mean).transpose();
+        pose_cov += (pose_array_filter.size() - 1) * (xi - xi_mean) * (xi - xi_mean).transpose();
+    }
 
-     std::cout << "calib mean: " << pose_mean << std::endl;
-     std::cout << "calib cov: " << std::endl
-               << pose_cov << std::endl;
-     // LOG(INFO) << "calib mean: " << pose_mean;
-     // LOG(INFO) << "calib cov: " << std::endl
-     //           << pose_cov;
+    std::cout << "calib mean: " << pose_mean << std::endl;
+    std::cout << "calib cov: " << std::endl
+              << pose_cov << std::endl;
+    // LOG(INFO) << "calib mean: " << pose_mean;
+    // LOG(INFO) << "calib cov: " << std::endl
+    //           << pose_cov;
 }
 
 //
